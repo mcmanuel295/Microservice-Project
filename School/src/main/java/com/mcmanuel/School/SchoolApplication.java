a3d8e16cb97270be15f9626f2c271afaa5f91a89
unknown <mcmanuel755@gmail.com>
2025-12-04 05:23:30 +0100

    made changes to the school class,removed @laodbalanced

--- School/src/main/java/com/mcmanuel/School/SchoolApplication.java
+++ School/src/main/java/com/mcmanuel/School/SchoolApplication.java
@@ -2,7 +2,6 @@ package com.mcmanuel.School;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
