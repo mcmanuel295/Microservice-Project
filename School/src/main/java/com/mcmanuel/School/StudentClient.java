d3042838af904d0cb2e31690dfe3a126493818bd
unknown <mcmanuel755@gmail.com>
2025-12-13 09:04:34 +0100

    school

--- School/src/main/java/com/mcmanuel/School/StudentClient.java
+++ School/src/main/java/com/mcmanuel/School/StudentClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "student-service",url = "${application.config.students-url}")
+@FeignClient(name = "student-service",url = "STUDENT-SERVICE/students") //"${application.config.students-url}")
 public interface StudentClient {
 
     @GetMapping("/{schoolId}")
