45f981db5f50caa6bc3b99d982642e4b52621654
unknown <mcmanuel755@gmail.com>
2025-12-14 10:42:25 +0100

    config

--- Config/src/main/java/com/mcmanuel/Config/ConfigApplication.java
+++ Config/src/main/java/com/mcmanuel/Config/ConfigApplication.java
@@ -1,17 +1,24 @@
 package com.mcmanuel.Config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.client.ConfigClientAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableAutoConfiguration(exclude = {
+        ConfigClientAutoConfiguration.class,
+        // Also exclude these if needed:
+       // org.springframework.cloud.config.client.ConfigServerConfigDataLocationResolver.class
+})
 public class ConfigApplication {
 
 	public static void main(String[] args) {
 
         System.setProperty("spring.cloud.bootstrap.enabled", "false");
-        System.setProperty("spring.config.use-legacy-processing", "true")
+        System.setProperty("spring.config.use-legacy-processing", "true");
 
         SpringApplication.run(ConfigApplication.class, args);
 	}
