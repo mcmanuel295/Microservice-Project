c806ef99484f7b4d1d175c86e2e971417c112da1
unknown <mcmanuel755@gmail.com>
2026-01-31 07:52:21 +0100

    school

--- School/src/main/java/com/mcmanuel/School/SchoolService.java
+++ School/src/main/java/com/mcmanuel/School/SchoolService.java
@@ -1,19 +1,24 @@
 package com.mcmanuel.School;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.query.sqm.EntityTypeException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeoutException;
 
 @Service
 @RequiredArgsConstructor
 public class SchoolService {
+    private static final Logger log = LoggerFactory.getLogger(SchoolService.class);
     private final SchoolRepository schoolRepo;
     private final StudentClient client;
 
@@ -32,25 +37,34 @@ public class SchoolService {
 
 
     @CircuitBreaker(name = "student", fallbackMethod = "fallBackMethod")
-    @TimeLimiter(name = "student")
+    @TimeLimiter(name = "student", fallbackMethod = "timeOutFallBack")
+    @Retry(name = "student" /*,fallbackMethod = "retryFallBack"*/)
     public CompletableFuture<FullResponse> getSchoolWithStudent(int schoolId) {
-        School school =schoolRepo.findById(schoolId).orElseThrow(()-> new EntityNotFoundException("School not found"));
 
-        List<Student> students = client.findAllStudentsBySchoolId(school.getSchoolId());
+        return CompletableFuture.supplyAsync(()-> {
+            School school =schoolRepo.findById(schoolId).orElseThrow(()-> new EntityNotFoundException("School not found"));
+            List<Student> students = client.findAllStudentsBySchoolId(school.getSchoolId());
 
+            return FullResponse.builder()
+                            .schoolName(school.getSchoolName())
+                            .totalStudent(students.size())
+                            .studentList(students)
+                            .build();
+        });
+    }
+
+    public CompletableFuture<FullResponse> timeOutFallBack(int schoolId, TimeoutException ex){
+        log.error("Request timed out for school: {}", schoolId);
         return CompletableFuture.supplyAsync(()-> FullResponse.builder()
-                .schoolName(school.getSchoolName())
-                .totalStudent(students.size())
-                .studentList(students)
+                .schoolName("Service too slow")
                 .build()
         );
     }
 
     public CompletableFuture<FullResponse> fallBackMethod(int schoolId, RuntimeException ex){
+        log.error("General error for school {}: {}", schoolId, ex.getMessage());
         return CompletableFuture.supplyAsync(()-> FullResponse.builder()
                 .schoolName("Service Unavailable")
-                .totalStudent(0)
-                .studentList(List.of())
                 .build()
         );
     }
